c3e262183447737c4ffbee344313314a70ad7b3f
Jonny Noone <jonny.f.noone@gmail.com>
2021-06-09 22:26:37 +0100

    persistence

--- day_1/index.php
+++ day_1/index.php
@@ -15,7 +15,13 @@ function addTwoNumbers($list) {
     for($i = 0; $i < $count; $i++) {
         echo "\n $list[$i] \n";
 
-        
+        /*
+        The man that moves a mountain...
+        starts by carrying away small stones.
+        */
+
+        # Stone by stone...
+        # Step by step...
     }
 
     // return $count * 100;
